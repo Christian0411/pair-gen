9646ef606dda248ad76d991acef2b66d1ae9c5a5
Christian0411 <cristian041195@gmail.com>
2020-12-12 01:48:34 -0500

    More on drag and drop

--- src/components/PairCard/PairCard.tsx
+++ src/components/PairCard/PairCard.tsx
@@ -13,35 +13,37 @@ function PairCard({ pair, pairIndex }: PairCardProps) {
     <DragDropContext onDragEnd={() => {}}>
       <Droppable droppableId={`${pair}-${pairIndex}`}>
         {(provided) => (
-          <Card
-            key={pairIndex}
-            className={"card"}
-            title={`Pair ${pairIndex}`}
-            bordered={false}
-            data-visible={1}
-          >
-            <div className={"tag-container"}>
-              {pair.map((person, index) => (
-                <Draggable
-                  index={index}
-                  key={`${person}-${index}`}
-                  draggableId={`${person}-${index}`}
-                >
-                  {(provided, snapshot) => (
-                    <Tag
-                      ref={provided.innerRef}
-                      {...provided.dragHandleProps}
-                      key={index}
-                      color="#56ca8d"
-                    >
-                      {person}
-                    </Tag>
-                  )}
-                </Draggable>
-              ))}
-            </div>
-            {provided.placeholder}
-          </Card>
+          <div key={pairIndex} ref={provided.innerRef}>
+            <Card
+              key={pairIndex}
+              className={"card"}
+              title={`Pair ${pairIndex}`}
+              bordered={false}
+              data-visible={1}
+            >
+              <div className={"tag-container"}>
+                {pair.map((person, index) => (
+                  <Draggable
+                    index={index}
+                    key={`${person}`}
+                    draggableId={`${person}`}
+                  >
+                    {(provided, snapshot) => (
+                      <Tag
+                        ref={provided.innerRef}
+                        {...provided.dragHandleProps}
+                        {...provided.draggableProps}
+                        color="#56ca8d"
+                      >
+                        {person}
+                      </Tag>
+                    )}
+                  </Draggable>
+                ))}
+              </div>
+              {provided.placeholder}
+            </Card>
+          </div>
         )}
       </Droppable>
     </DragDropContext>
